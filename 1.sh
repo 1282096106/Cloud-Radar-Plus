eb8c76e2a20a481f1792e96629d8b76b8dd7ca2f
1282096106 <38853326+1282096106@users.noreply.github.com>
2018-05-03 11:54:43 +0800

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "欢迎使用一键搭建 脚本"
-echo "即将搭建的是Xiaohua Cloud Radar v1.2+版本"
+echo "即将搭建的是鬼迹 Cloud Radar v1.2+版本"
 echo "准备开始安装"
 read -p "回车后开始安装"
 echo "请输入 你服务器的 内网ip" 
@@ -26,7 +26,7 @@ cd libpcap-1.8.1
 make
 make install
 
-git clone https://github.com/XiaohuaCN/Cloud-Radar-Plus.git
+git clone https://github.com/1282096106/Cloud-Radar-Plus.git
 cd Cloud-Radar-Plus/
 npm i
 npm i -g pino
